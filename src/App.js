7df178cac0df236b752f2ebbad2896546b1aa7fc
johnson7m <155421182+johnson7m@users.noreply.github.com>
2024-09-14 23:14:27 -0400

    Calculator 2.0!
    Overhauled design for calculator. 
    Mobile first development. 
    R.I.P. 1.0

--- src/App.js
+++ src/App.js
@@ -1,89 +1,97 @@
-import React from 'react'
-import { Numpad } from './Numpad.js'
+import React from 'react';
+import { Numpad } from './Numpad.js';
 import { evaluate } from 'mathjs';
-
 
 export class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       displayText: '0',
       displayArray: [],
       mathArray: [],
       outputText: '',
       acceptDecimalInput: true,
-    }
+    };
     this.evaluateMath = this.evaluateMath.bind(this);
     this.handleClear = this.handleClear.bind(this);
     this.handleClick = this.handleClick.bind(this); 
     this.appendDecimal = this.appendDecimal.bind(this);
   }
 
+  
+
 evaluateMath() {
+  try {
     let mathString = this.state.outputText;
-    
-    // Handle consecutive operators, keeping the last operator and the negative sign if present
     mathString = mathString.replace(/([+\-*/])+/g, (match, p1) => p1 === '-' && match.length > 1 ? match.slice(-2) : p1);
-
     const result = evaluate(mathString);
+
+    // Limit result length for display
+    const limitedResult = result.toString().length > 12 ? result.toExponential(5) : result.toString();
+
     this.setState({
-        displayText: result.toString(),
-        outputText: mathString + ` = ${result}`,
-        mathArray: [result.toString()],
-        displayArray: [result.toString()],
+      displayText: limitedResult,
+      outputText: mathString + ` = ${limitedResult}`,
+      mathArray: [limitedResult],
+      displayArray: [limitedResult],
     });
+  } catch (error) {
+    this.setState({
+      displayText: 'Error',
+      outputText: '',
+      mathArray: [],
+      displayArray: [],
+    });
+  }
 }
 
+handleClick(event) {
+  const input = event.target.innerText;
+  const isDigit = !isNaN(input);
 
-    handleClick(event) {
-    const input = event.target.innerText;
-    const isDigit = !isNaN(input);
+  // Prevent expanding the width of the calculator
+  if (this.state.displayArray.join('').length > 12) {
+    return; // Prevent further input if the display limit is reached
+  }
 
-    if (isDigit && this.state.displayText === '0') {
-        this.setState({
-            displayText: input,
-            outputText: input,
-            mathArray: [input],
-            displayArray: [input],
-        });
-    } else {
-        if (input === '+' || input === '-' || input === '*' || input === '/') {
-            this.setState({
-                acceptDecimalInput: true,
-            });
-        }
-        this.state.mathArray.push(input);
-        this.state.displayArray.push(input);
-        this.setState({
-            displayText: this.state.displayArray.join(''),
-            outputText: this.state.mathArray.join(''),
-        });
+  if (isDigit && this.state.displayText === '0') {
+    this.setState({
+      displayText: input,
+      outputText: input,
+      mathArray: [input],
+      displayArray: [input],
+    });
+  } else {
+    if (['+', '-', '*', '/'].includes(input)) {
+      this.setState({
+        acceptDecimalInput: true,
+      });
     }
+    this.state.mathArray.push(input);
+    this.state.displayArray.push(input);
+    this.setState({
+      displayText: this.state.displayArray.join(''),
+      outputText: this.state.mathArray.join(''),
+    });
+  }
 }
 
-
-
-appendDecimal(event) {
+  appendDecimal(event) {
     const input = event.target.innerText;
-    // Find the index of the last operator in the mathArray
     const lastOperatorIndex = this.state.mathArray.slice().reverse().findIndex(item => isNaN(item) && item !== '.');
-    // Calculate the index of the last number in the mathArray
     const lastNumberIndex = lastOperatorIndex >= 0 ? this.state.mathArray.length - lastOperatorIndex - 1 : 0;
-    // Get the last number in the mathArray
     const lastNumber = this.state.mathArray.slice(lastNumberIndex).join('');
 
-    // Only add a decimal point if the last number does not already contain one
     if (!lastNumber.includes('.')) {
-        this.state.mathArray.push(input);
-        this.state.displayArray.push(input);
-        this.setState({
-            displayText: this.state.displayArray.join(''),
-            outputText: this.state.mathArray.join(''),
-            acceptDecimalInput: false,
-        });
+      this.state.mathArray.push(input);
+      this.state.displayArray.push(input);
+      this.setState({
+        displayText: this.state.displayArray.join(''),
+        outputText: this.state.mathArray.join(''),
+        acceptDecimalInput: false,
+      });
     }
-}
-
+  }
 
   handleClear() {
     this.setState({
@@ -91,19 +99,24 @@ appendDecimal(event) {
       outputText: '',  
       mathArray: [],
       displayArray: [],
-    })
+    });
   }
 
   render() {
     return (
-      <div>
-        <div className="flex-container">
-            <p id="output">{this.state.outputText}</p>
-            <h3 id="display" className="input">{this.state.displayText}</h3>
+      <div className="container">
+        <div className="display-container">
+          <p className="output-text">{this.state.outputText}</p>
+          <h3 id="display" className="display-text">{this.state.displayText}</h3>
         </div>
-        <Numpad appendDecimal={this.appendDecimal} evaluateMath={this.evaluateMath} handleClear={this.handleClear} handleClick={this.handleClick}/>
+        <Numpad 
+          appendDecimal={this.appendDecimal} 
+          evaluateMath={this.evaluateMath} 
+          handleClear={this.handleClear} 
+          handleClick={this.handleClick} 
+        />
       </div>
-    )
+    );
   }
 }
 
--- src/Numpad.js
+++ src/Numpad.js
@@ -1,31 +1,27 @@
 import React from "react";
 
 export class Numpad extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-
-    render() {
-        return(
-            <div className="parent">
-            <button onClick={this.props.handleClick} id="one" className="div1">1</button>
-            <button onClick={this.props.handleClick} id="two" className="div2">2</button>
-            <button onClick={this.props.handleClick} id="three" className="div3">3</button>
-            <button onClick={this.props.handleClick} id="four" className="div4">4</button>
-            <button onClick={this.props.handleClick} id="five" className="div5">5</button>
-            <button onClick={this.props.handleClick} id="six" className="div6">6</button>
-            <button onClick={this.props.handleClick} id="seven" className="div7">7</button>
-            <button onClick={this.props.handleClick} id="eight" className="div8">8</button>
-            <button onClick={this.props.handleClick} id="nine" className="div9">9</button>
-            <button onClick={this.props.handleClick} id="zero" className="div10">0</button>
-            <button onClick={this.props.appendDecimal} id="decimal" className="div11">.</button>
-            <button onClick={this.props.evaluateMath} id="equals" className="div12">=</button>
-            <button onClick={this.props.handleClick} id="subtract" className="div13">-</button>
-            <button onClick={this.props.handleClick} id="add" className="div14">+</button>
-            <button onClick={this.props.handleClick} id="multiply" className="div15">*</button>
-            <button onClick={this.props.handleClick} id="divide" className="div16">/</button>
-            <button onClick={this.props.handleClear} id="clear" className="div17">AC</button>
-          </div> 
-        )
-    }
+  render() {
+    return (
+      <div className="numpad-container">
+        <button onClick={this.props.handleClick} id="one" className="number">1</button>
+        <button onClick={this.props.handleClick} id="two" className="number">2</button>
+        <button onClick={this.props.handleClick} id="three" className="number">3</button>
+        <button onClick={this.props.handleClick} id="four" className="number">4</button>
+        <button onClick={this.props.handleClick} id="five" className="number">5</button>
+        <button onClick={this.props.handleClick} id="six" className="number">6</button>
+        <button onClick={this.props.handleClick} id="seven" className="number">7</button>
+        <button onClick={this.props.handleClick} id="eight" className="number">8</button>
+        <button onClick={this.props.handleClick} id="nine" className="number">9</button>
+        <button onClick={this.props.handleClick} id="zero" className="number">0</button>
+        <button onClick={this.props.appendDecimal} id="decimal" className="number">.</button>
+        <button onClick={this.props.evaluateMath} id="equals" className="operator">=</button>
+        <button onClick={this.props.handleClick} id="subtract" className="operator">-</button>
+        <button onClick={this.props.handleClick} id="add" className="operator">+</button>
+        <button onClick={this.props.handleClick} id="multiply" className="operator">*</button>
+        <button onClick={this.props.handleClick} id="divide" className="operator">/</button>
+        <button onClick={this.props.handleClear} id="clear" className="clear">AC</button>
+      </div>
+    );
+  }
 }
